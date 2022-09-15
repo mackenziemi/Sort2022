8b0699b5f46e64a979bdd568c7f72f1511c6560a
MacKenzie Mickelsen <mackenzie.mickelsen@churchofjesuschrist.org>
2022-09-14 18:36:55 -0600

    Coding against a POST

--- Sort2022/Sort2022/Program.cs
+++ Sort2022/Sort2022/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Sort2022.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Welcome to SORT 2022");
+app.MapGet("/", () => "Welcome to SORT 2022");
+
+app.MapPost("/user", ([FromBody] User user, HttpResponse response) =>
+{
+    Console.WriteLine("/users was called!");
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Username: {user.Username}, Password: {user.Password}");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    response.StatusCode = 201;
+
+    return user;    
+})
+    .Produces<User>();
+
+
+app.Urls.Add("http://localhost:5757");
 
 app.Run();
